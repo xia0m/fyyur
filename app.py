4930bd453f4a854445e86b9f87fa491e433dff3a
xia0m <alexma325@gmail.com>
2019-12-10 18:35:40 -0800

    feat: display default value in venue form

--- app.py
+++ app.py
@@ -253,22 +253,22 @@ def edit_artist_submission(artist_id):
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
   form = VenueForm()
-  venue={
-    "id": 1,
-    "name": "The Musical Hop",
-    "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-    "address": "1015 Folsom Street",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "123-123-1234",
-    "website": "https://www.themusicalhop.com",
-    "facebook_link": "https://www.facebook.com/TheMusicalHop",
-    "seeking_talent": True,
-    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-  }
+  
   # TODO: populate form with values from venue with ID <venue_id>
-  return render_template('forms/edit_venue.html', form=form, venue=venue)
+  venue_data = Venue.query.get(venue_id)
+  form.name.default = venue_data.name
+  form.city.default = venue_data.city
+  form.state.default = venue_data.state
+  form.address.default = venue_data.address
+  form.phone.default = venue_data.phone
+  form.genres.default = venue_data.genres
+  form.website.default = venue_data.website
+  form.facebook_link.default = venue_data.facebook_link
+  form.image_link.default = venue_data.image_link
+  form.seeking_talent.default = "Yes" if venue_data.seeking_talent==True else "No"
+  form.seeking_description.default = venue_data.seeking_description
+  form.process()
+  return render_template('forms/edit_venue.html', form=form, venue=venue_data)
 
 @app.route('/venues/<int:venue_id>/edit', methods=['POST'])
 def edit_venue_submission(venue_id):
