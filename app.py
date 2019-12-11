0a329551d75f5c25533e830b8170462be2990db2
xia0m <alexma325@gmail.com>
2019-12-11 01:25:43 -0800

    feat:added edit and delete button, implement delete venue functionality

--- app.py
+++ app.py
@@ -186,10 +186,20 @@ def create_venue_submission():
 def delete_venue(venue_id):
   # TODO: Complete this endpoint for taking a venue_id, and using
   # SQLAlchemy ORM to delete a record. Handle cases where the session commit could fail.
+  try:
+    venue = Venue.query.get(venue_id)
+    db.session.delete(venue)
+    db.session.commit()
+    flash("The venue is successfully deleted from database!")
+  except:
+    db.session.rollback()
+    flash("Error deleting the venue!")
+  finally:
+    db.session.close()
 
   # BONUS CHALLENGE: Implement a button to delete a Venue on a Venue Page, have it so that
   # clicking that button delete it from the db then redirect the user to the homepage
-  return None
+  return redirect(url_for('index'))
 
 #  Artists
 #  ----------------------------------------------------------------
