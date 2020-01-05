f1b1a0aeca54c4d6bb7f56faba07370ed9d6f692
xia0m <alexma325@gmail.com>
2020-01-04 19:59:38 -0800

    fixed: format of forms.py

--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from flask_wtf import Form
-from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField, BooleanField
+from wtforms import StringField, SelectField, SelectMultipleField,\
+    DateTimeField, BooleanField
 from wtforms.validators import DataRequired, AnyOf, URL
 
 
